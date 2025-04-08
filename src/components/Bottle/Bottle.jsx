e1f22a3a22ae3c76435fde5ed9e90560f92a291b
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-04-08 13:25:46 +0600

    Cart

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Bottle.css'
 
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle,handleAddToCart}) => {
+
     console.log(bottle)
+    const {name,img,price,stock}= bottle
+
     return (
-        <div className='card'>
-            <img src={bottle.img} alt="" />
+        <div className='card bottle'>
+           <img src={img} alt="" />
+            <p>{name}</p>
+            <p>${price}</p>
+            <p>Remaining Bottle: {stock}</p>
+            <button onClick={()=>handleAddToCart(bottle)}>Buy Now</button>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,18 +1,39 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Bottle from '../Bottle/Bottle';
+import './Bottles.css'
 
 const Bottles = ({ bottlesPromise }) => {
     const bottles = use(bottlesPromise)
     // console.log(bottles)
+
+    const [cart, setCart] = useState([])
+
+    const handleAddToCart=(bottle)=>{
+         console.log('Add to cart',bottle)
+        const newCart = [...cart, bottle]
+        setCart(newCart)
+    }
+
     return (
         <div>
             <p>{bottles.length}</p>
+            <p>Cart Items: {cart.length}</p>
             <div>
+                {
+                    cart.map((c)=>{
+                        return(
+                            <div key={c.id}>
+                             <img src={c.img} alt="" />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className='bottles-container'>
                 {
                     bottles.map((bottle) => {
                         return (
-                            <Bottle key={bottle.id} bottle={bottle}>
-
+                            <Bottle key={bottle.id} bottle={bottle} handleAddToCart={handleAddToCart}>
                             </Bottle>
                         )
                     })
